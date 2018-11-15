5d8875294193776f34aeb168a21297b3b64034ef
Dom_Portable <freedomzelda_dc@hotmail.com>
2018-11-15 08:54:32 +0800

    Change to third-party BigInteger library
    Third-party BigInteger library taken from https://www.codeproject.com/Articles/2728/C-BigInteger-Class

--- Assets/PrettyBigInteger/Core/PrettyBigInteger.cs
+++ Assets/PrettyBigInteger/Core/PrettyBigInteger.cs
@@ -1,66 +1,11 @@
 ﻿namespace Bannified
 {
-	using System.Numerics;
     using System;
     
     [Serializable]
-	public class PrettyBigInteger //: IFormattable, IComparable, IComparable<PrettyBigInteger>, IEquatable<PrettyBigInteger>
+	public class PrettyBigInteger : BigInteger
 	{
-        public BigInteger Value;
 
-        #region Operator Overloads
-        
-        
-        public static PrettyBigInteger operator +(PrettyBigInteger value1, int value2)
-        {
-            return new PrettyBigInteger(value1.Value + value2);
-        }
-        
-        public static PrettyBigInteger operator /(PrettyBigInteger value1, PrettyBigInteger value2)
-        {
-            return new PrettyBigInteger(value1.Value / value2.Value);
-        }
-        
-        public static PrettyBigInteger operator *(PrettyBigInteger value1, PrettyBigInteger value2)
-        {
-            return new PrettyBigInteger(value1.Value * value2.Value);
-        }
-        
-        public static PrettyBigInteger operator -(PrettyBigInteger value1, PrettyBigInteger value2)
-        {
-            return new PrettyBigInteger(value1.Value - value2.Value);
-        }
-        
-        public static PrettyBigInteger operator +(PrettyBigInteger value1, PrettyBigInteger value2)
-        {
-            return new PrettyBigInteger(value1.Value + value2.Value);
-        }        
-        
-        public static implicit operator PrettyBigInteger(int value)
-        {
-            return new PrettyBigInteger(value);
-        }
-        
-        public static implicit operator PrettyBigInteger(BigInteger value) {
-            return new PrettyBigInteger(value);
-        }
-        
-        public static explicit operator BigInteger(PrettyBigInteger pbi) {
-            return pbi.Value;
-        }
-
-        #endregion
-        
-        #region Constructors
-        
-        public PrettyBigInteger(int integer) : this(new BigInteger(integer)) {
-        }
-        
-        private PrettyBigInteger(BigInteger bigInt) {
-            Value = bigInt;
-        }
-        
-        #endregion
 
     }
 }
