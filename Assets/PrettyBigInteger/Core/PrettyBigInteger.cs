b233d70de96164cea4f137a4a845a4d21a487a07
Dominic Chong <freedomzelda_dc@hotmail.com>
2018-11-16 14:52:56 +0800

    Setup implementation of PBI

--- Assets/PrettyBigInteger/Core/PrettyBigInteger.cs
+++ Assets/PrettyBigInteger/Core/PrettyBigInteger.cs
@@ -1,11 +1,18 @@
 ﻿namespace Bannified
 {
     using System;
-    
-    [Serializable]
+
+	[Serializable]
 	public class PrettyBigInteger : BigInteger
 	{
+		public override string ToString()
+		{
+			return base.ToString();
+		}
 
+		public PrettyBigInteger(long value) : base(value)
+		{
 
-    }
+		}
+	}
 }
