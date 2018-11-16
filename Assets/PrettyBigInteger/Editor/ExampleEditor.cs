a7557a31bade7563f94daabdf8942fef4beded6a
Dominic Chong <freedomzelda_dc@hotmail.com>
2018-11-16 14:45:22 +0800

    Add simple Example to test for output based on input long value

---
+++ Assets/PrettyBigInteger/Editor/ExampleEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Example))]
+public class ExampleEditor : Editor {
+
+	long inputValue = 0;
+
+	public override void OnInspectorGUI()
+	{
+		Example script = (Example)target;
+
+		inputValue = EditorGUILayout.LongField("INPUT TEST VALUE HERE", inputValue);
+
+		if (GUILayout.Button("Convert to Strings")) {
+			script.updateExample(inputValue);
+		}
+		base.OnInspectorGUI();
+	}
+}
---
+++ Assets/PrettyBigInteger/Examples/Example/Example.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour {
+	
+	public Bannified.PrettyBigInteger prettyBigNumber;
+
+	public string normalString;
+
+	public string outputPrettyString;
+
+	public void updateExample(long inputNumber) {
+		BigInteger big = new BigInteger(inputNumber);
+		normalString = big.ToString();
+
+		Bannified.PrettyBigInteger prettyBig = new Bannified.PrettyBigInteger(inputNumber);
+		prettyBigNumber = prettyBig;
+		outputPrettyString = prettyBig.ToString();
+	}
+}
