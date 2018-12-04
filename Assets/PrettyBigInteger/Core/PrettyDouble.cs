d056b331f9d56fee6c4f5d74456a217fa349a99b
Dominic Chong <freedomzelda_dc@hotmail.com>
2018-12-04 15:07:26 +0800

    Change PrettyDouble to static class

--- Assets/PrettyBigInteger/Core/PrettyDouble.cs
+++ Assets/PrettyBigInteger/Core/PrettyDouble.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 
 namespace Bannified {
-	public class PrettyDouble
+	public static class PrettyDouble
 	{
 		public static readonly string[] standardSuffixesSet = { " ", "K", "M", "B", "T" };
 
 		const string specialSuffixCharSet = " abcdefghijklmnopqrstuvwxyxABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-		const int numberOfDec = 2;
+		const int numberOfDecimals = 2;
 
+		/// <summary>
+		/// Returns the Pretty-formatted (max 5 digits) BigInteger.
+		/// 
+		/// The Pretty format is as follows:
+		/// - No more than 5 digits at any time
+		/// - Fractional part is always to 2 most significant numbers.
+		/// - Follows the Short scale pattern, where the difference in order of magnitude
+		/// between a number and another of a scale greater is 3.
+		/// </summary>
+		/// <returns>Pretty-formatted (max 5 digits) BigInteger.</returns>
 		public static string DoubleToPrettyDouble(double value)
 		{
 			string fullString = value.ToString("F0");
@@ -22,7 +32,7 @@ namespace Bannified {
 			int numberOfWhole = (fullString.Length - 1) % 3 + 1;
 
 			string number = (fullString.Length > 3) ?
-				String.Join(".", fullString.Substring(0, numberOfWhole), fullString.Substring(numberOfWhole, numberOfDec)) :
+				String.Join(".", fullString.Substring(0, numberOfWhole), fullString.Substring(numberOfWhole, numberOfDecimals)) :
 				String.Join(".", fullString.Substring(0, numberOfWhole));
 
 			result = number;
