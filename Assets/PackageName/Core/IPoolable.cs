1a81e5329806b8b7bd5848cfbbf525199a7f8b90
bannified <freedomzelda_dc@hotmail.com>
2019-01-18 21:35:45 +0800

    Import object pool scripts used in old projects

---
+++ Assets/PackageName/Core/IPoolable.cs
@@ -0,0 +1,10 @@
+﻿// An interface for all poolable objects
+public interface IPoolable {
+
+    // This method has to be defined in a component that
+    // has properties that require resetting upon reuse
+    void OnReuse();
+
+    void OnReuseWithArguments(params object[] list);
+
+}
---
+++ Assets/PackageName/Core/PoolableObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PoolableObject : MonoBehaviour {
+    
+	public void SetProperties()
+    {
+
+    }
+
+    protected virtual void OnEnable()
+    {
+
+    }
+
+    protected virtual void OnDisable()
+    {
+
+    }
+
+}
