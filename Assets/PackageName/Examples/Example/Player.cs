9390016e2b9c272d27c3aed7e21a7c254950da6d
bannified <freedomzelda_dc@hotmail.com>
2019-01-18 23:13:33 +0800

    Add example Player script.

---
+++ Assets/PackageName/Examples/Example/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Glazed.Optimization;
+
+namespace Glazed.Example
+{
+	public class Player : MonoBehaviour
+	{
+		[SerializeField]
+		GameObject projectilePrefab;
+
+		void Start()
+		{
+			PoolManager.instance.CreatePool(projectilePrefab, 100);
+		}
+
+		private void Update()
+		{
+			if (Input.GetButton("Fire"))
+			{
+				Shoot();
+			}
+		}
+
+		void Shoot()
+		{
+
+		}
+
+	}
+}
